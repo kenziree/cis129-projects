91a348ddc26dce568776845b3385bde4a0c991c7
Kenzi Marie <mtitus@mail.pima.edu>
2024-07-16 10:45:07 -0700

    Add files via upload

---
+++ 9.1.py
@@ -0,0 +1,27 @@
+#Makenzi Titus
+#Module 11 Lab
+#figure 9.1
+
+#function to store grades in grades.txt
+def store_grades():
+    with open('grades.txt', 'w') as file:
+        while True:
+            grade = input("Enter a grade (or -1 to finish): ")
+            if grade == '-1':
+                break
+            try:
+                float(grade) #checks if input is valid number
+                file.write(grade = '\n')
+            except ValueError:
+                print("Invalid input. Please enter a valid nukber grade.")
+    print("Grades have been saved to grades.txt")
+
+#Store grades
+store_grades()
+
+#verify grades.txt
+print("\n Grades stored in file:")
+with open('grades.txt', 'r') as file: 
+    grades = file.readlines()
+    for grade in grades:
+        print(grade.strip())
---
+++ 9.3.py
@@ -0,0 +1,36 @@
+#Makenzi Titus
+#Module 11 Lab
+#Exercise 9.3
+
+import csv
+
+#Function to store grades in grades.csv
+def store_student_grades():
+    #open csv file
+    with open('grades.csv', 'w', newline-'') as csvfile:
+        #create csv writer object
+        grade_writer = csv.writer(csvfile)
+
+        #Header row
+        grade_writer.writerow(('firstname', 'lastname', 'exam1grade', 'exam2grade', 'exam3grade'))
+        
+        while True:
+            firstname = input("Enter students first name (or 'done' to finish.)")
+            if firstname.lower() == 'done':
+                break
+            lastname = input("Enter students last name: ")
+            try:
+                exam1 = int(input("Enter first exam grade: "))
+                exam2 = int(input("Enter second exam grade: "))
+                exam3 - int(input("Enter third exam grade: "))
+            except ValueError:
+                print("Invalid input. Please eneter a valid number grade.")
+                continue
+
+            #write student info to csv file
+            grade_writer.writerow([firstname, lastname, exam1, exam2, exam3])
+
+    print("Student grades have been saved to grade.csv")
+#store grades
+store_student_grades()
+        
